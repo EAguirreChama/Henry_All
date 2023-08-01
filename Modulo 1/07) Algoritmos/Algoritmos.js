f45dc7ab4def53181db7bbc8e3efc49a3319986c
EAguirre <aguirreernesto25@gmail.com>
2023-08-01 19:58:40 -0300

    Modulo 1 - Clase 7

---
+++ Modulo 1/07) Algoritmos/Algoritmos.js
@@ -0,0 +1,12 @@
+const Array1 = [1,2,3,4,5,6]
+const Array2 = [1,2,3,4,5,6,7]
+
+for (let i = 0 ; i < Array1.length ; i++) {
+    for (let j = 0 ; j < Array1.length ; j++) {
+        console.log(Array1[j])
+    }
+}
+
+// Complejidad se representa
+// O (N(representa la cantidad de pasos de Array1) * M(representa la cantidad de pasos de Array2))
+// O (N * M) => (6 * 7) = 42

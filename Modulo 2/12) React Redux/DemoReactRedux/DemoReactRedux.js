5eda6b2b16c07bccae902aaeef37eea79e2d5bb5
EAguirre <aguirreernesto25@gmail.com>
2023-08-03 22:10:36 -0300

    Modulo 2 - Clase 12

---
+++ Modulo 2/12) React Redux/DemoReactRedux/DemoReactRedux.js
@@ -0,0 +1,9 @@
+// React Redux 
+
+// Componentes
+// 
+// Dumb ==> componente tonto
+// Solo muestran informacion, trabaja con las props. No tienen estado propio. Trabajan con la presentación.
+// 
+// Smart ==> componente inteligente
+// Como funcionan las cosas. Sin estilos. Provee datos. Invoca acciones. Trabajan con la logica

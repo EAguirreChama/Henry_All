10f7c3c4dc1c2c23bd21430594ccbfbc46c5eaeb
EAguirre <aguirreernesto25@gmail.com>
2023-08-03 22:04:47 -0300

    Modulo 2 - Clase 10

---
+++ Modulo 2/10) React Parte 05/DemoReactForms/DemoReactForms.js
@@ -0,0 +1,7 @@
+// Formularios
+// Dos tipos de formularios
+// Controlados y NoControlados. Nosotros vamos a trabajar con los controlados
+
+// Controlados
+// Es un metodo de Http llamado Post
+// La etiqueta form es para formularios

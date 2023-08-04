8551b7b5fe123fc0c485acdf4a1e2d89dbab018c
EAguirre <aguirreernesto25@gmail.com>
2023-08-03 21:55:55 -0300

    Modulo 2 - Clase 6

---
+++ Modulo 2/06) React Parte 01/DemoReactIntro/DemoReactIntro.js
@@ -0,0 +1,42 @@
+// React
+// Es una libreria para el Front
+// React es declarativo
+// Trae el VirtualDom
+// Trabaja con ES6
+// Para abrir la aplicacion ==> npm start
+// Para trabajar con clases debo importar React de React
+// Se usa la palabra reservada extends React.Component para saber que es una clase de React
+
+
+// Para usar React se instala
+// npx create-react-app y el nombre de la carpeta que se va a crear
+// Su estructura
+// Carpeta public ==> un html
+// Carpeta src ==> dos archivos de css, app.js y index.js
+// index.js es un entry point
+
+
+// Virtual Dom
+
+// Tipos de programacion
+// Imperativo ==> como hacer algo paso a paso
+// Declarativo ==> no especifica el paso a paso
+
+// Componentes
+// Es una especie de plantilla 
+// Es una unidad modular que permite dividir las tareas en la interfaz de usuario
+// Cada archivo se llama componente
+// Son independientes y realizan una unica tarea. Se los exporta para que se los pueda usar
+// Todo lo que creemos en los componentes debe ser dentro de una sola etiqueta
+// Las props se pasan como parametro
+// Para escribir codigo de js dentro de una etiqueta se tienen que abrir {}
+
+// Tenemos dos tipos de componentes
+// Funcional
+// Clases
+
+// Jsx
+// Permite mezclar codigo de Js y Html en un componente
+
+// Babel
+// Nos permite pasar la informacion de una mejor manera

d7484875ae0d35ad7898276cb90f97ba0a773d37
EAguirre <aguirreernesto25@gmail.com>
2023-08-03 22:01:47 -0300

    Modulo 2 - Clase 9

---
+++ Modulo 2/09) React Parte 04/DemoReactRouting/DemoReactRouting.js
@@ -0,0 +1,26 @@
+// React Router Dom
+// Podremos poner toda la info en una sola pagina
+
+
+// Routing 
+// Es cuando dirijimos al usuario a otra URL
+// Tenemos que instalar la version 6 de react-router-dom@6.3.0 (siempre indicar la version que quiero instalar)
+// Para decirle a la aplicacion que vamos a usar rutas vamos a envolver a App con BrowserRouters en el index
+
+// Atributos del Routing
+// path ==> es la ruta que quiero poner(/algo)
+// element ==> es el componente que quiero renderizar
+// Si ponemos path=":algo" significa que es variable y sea lo que sea que diga despues de los : se va a mostrar ese componente.
+
+// Configuracion
+// BrowserRouter ==> permite mostrar todas las rutas
+// Routes ==> envuelve toda la ruta
+// Route ==> cada ruta(con path y element)
+
+// NavLink ==> Puedo darle estilos
+// Link ==> no puedo darle estilos
+
+// Hooks
+// useLocation ==> retorna un objeto
+// useNavigate ==> permite navegar (se lo usa en los button y se lo usa para no usar Link) Se coloca <button onClick={() => navigate('/')}>
+// useParams ==> es un objeto que devuelve la propiedad y su variable de la nav. El nombre que coloco luego de los :
---
+++ Modulo 2/09) React Parte 04/homework/01 - Exercises/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+/* eslint-disable */
+import Home from "./components/Home/Home.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Shipping from "./components/Shipping/Shipping.jsx";
+import Discounts from "./components/Discounts/Discounts.jsx";
+import CardDetail from "./components/CardDetail/CardDetail.jsx";
+import { Routes, Route} from "react-router-dom";
+/* eslint-disable */
+
+export default function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Routes>
+        <Route path="/" element={<Home/>}>Home</Route>
+        <Route path="/shipping" element={<Shipping/>}>Shipping</Route>
+        <Route path="/discounts" element={<Discounts/>}>Discounts</Route>
+        <Route path="/cruises/:id" element={<CardDetail/>}>CardDetail</Route>
+      </Routes>
+    </div>
+  )
+}

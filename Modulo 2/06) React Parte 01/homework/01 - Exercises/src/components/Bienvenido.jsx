d6be245ff95c9b02df111781964b530bd4131f6c
EAguirre <aguirreernesto25@gmail.com>
2023-08-03 21:56:54 -0300

    Modulo 2 - Clase 6

---
+++ Modulo 2/06) React Parte 01/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Botones from "./Botones";
+
+const studentName = "Ernesto";
+const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
+const alerts = { m1: "Aprobado", m2: "En curso" };
+
+export default function Bienvenido() {
+  // el código de tu componente acá
+  return (
+    <div>
+      <h1>Cursado de Soy Henry</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map ((tech) => {
+          return <li key={tech.id}>{tech}</li>
+        })}
+      </ul>
+      <Botones alerts={alerts}/>
+    </div>
+  )
+}
+
+// Esto lo exportamos para los tests
+export { studentName, techSkills, alerts };

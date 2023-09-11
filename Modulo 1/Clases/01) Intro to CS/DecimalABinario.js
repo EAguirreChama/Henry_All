5a6376d9f53bfc6745a8aefd07b636715deb538e
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-11 12:26:14 -0300

    Update DecimalABinario.js

--- Modulo 1/Clases/01) Intro to CS/DecimalABinario.js
+++ Modulo 1/Clases/01) Intro to CS/DecimalABinario.js
@@ -3,6 +3,5 @@
 // Decimal           8/2 = 4 => 4/2 = 2 => 2/2 = 1 => 1/2 = 0
 // Resto              0          0          0          1  => Se lo forma del ultimo al primero, es decir = 1000 (8 en Num Binario)
 
-
 // Decimal    13/2 = 6 => 6/2 = 3 => 3/2 = 1 => 1/2 = 0
 // Resto        1          0          1          1        => 1101 (13 en Num Binario) 

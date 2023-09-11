10598ecb1a4fe58c009875cd12c34e2218e9c5df
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-11 12:36:58 -0300

    Update Demo2(Closures).js

--- Modulo 1/Clases/03) Js Avanzado - Parte 2/Demo2(Closures).js
+++ Modulo 1/Clases/03) Js Avanzado - Parte 2/Demo2(Closures).js
@@ -1,12 +1,11 @@
 // Con VAR
-
-
 var CreaFuncion = function () {
-    var array = [] // [ 
+    var array = [] 
+// [ 
                 // function () { console.log(i);} (cuando i = 0) ; 
                 // function () { console.log(i);} (cuando i = 1) ; 
                 // function () { console.log(i);} (cuando i = 2) ;
-                   // ]
+// ]
 
     for (var i = 0 ; i < 3 ; i++) {
         // i = 0 (cuando inicia) hasta 3 se repite
@@ -22,10 +21,9 @@ var Funciones = CreaFuncion () // Si llamo una función tengo el valor de retorn
                 // function () { console.log(i);} (cuando i = 0) ; 
                 // function () { console.log(i);} (cuando i = 1) ; 
                 // function () { console.log(i);} (cuando i = 2) ;
-                   // ]
+// ]
 
 Funciones [0] () // Queda el valor i = 3
 Funciones [1] () // " "
 Funciones [2] () // " "
-
 // Sucede porque la variable es "VAR"

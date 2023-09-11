c6140dc12ad1dcd55a7399c3993568c8d3d0d76f
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-11 12:37:51 -0300

    Update Demo6(Call).js

--- Modulo 1/Clases/03) Js Avanzado - Parte 2/Demo6(Call).js
+++ Modulo 1/Clases/03) Js Avanzado - Parte 2/Demo6(Call).js
@@ -3,9 +3,7 @@ let persona = {
     nombre: "Ernesto",
     apellido: "Aguirre",
 }
-
 let logNombre = function () {
     console.log(this.nombre + " " + this.apellido);
 }
-
 logNombre.call(persona)

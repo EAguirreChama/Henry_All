12142a2c4a09d9e0eda1c95fdef0beeb4bd6291f
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-27 10:35:33 -0300

    Update 1).js

--- Modulo 1/Clases/02) Js Avanzado - Parte 1/Homework/Ejercicios 2/1).js
+++ Modulo 1/Clases/02) Js Avanzado - Parte 1/Homework/Ejercicios 2/1).js
@@ -1,19 +1,21 @@
 x = 1;
+
 var a = 5;
 var b = 10;
 var c = function (a, b, c) {
    var x = 10;
-   console.log(x);
-   console.log(a);
+   console.log(x)
+   console.log(a)
    var f = function (a, b, c) {
       b = a;
-      console.log(b);
+      console.log(b)
       b = c;
       var x = 5;
-   };
-   f(a, b, c);
-   console.log(b);
-};
-c(8, 9, 10);
-console.log(b);
-console.log(x);
+   }
+   f(a, b, c)
+   console.log(b)
+}
+
+c(8, 9, 10)
+console.log(b)
+console.log(x)

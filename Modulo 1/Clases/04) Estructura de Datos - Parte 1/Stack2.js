174ba95cc1c7354df44e185aa1e2686aa85785c0
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-28 19:46:04 -0300

    Update Stack2.js

--- Modulo 1/Clases/04) Estructura de Datos - Parte 1/Stack2.js
+++ Modulo 1/Clases/04) Estructura de Datos - Parte 1/Stack2.js
@@ -19,12 +19,10 @@ let NewStack = new Stack()
 NewStack.agregar(2)
 NewStack.agregar(10)
 NewStack.agregar(3)
-console.log(NewStack);
+console.log(NewStack)
 // NewStack.unshift(100) //Is not a function porque no esta dentro de mi prototipo
 
 //Para ver que estoy borrando deberia poner console.log antes
 NewStack.borrar()
 
-
-
-console.log(NewStack);
+console.log(NewStack)

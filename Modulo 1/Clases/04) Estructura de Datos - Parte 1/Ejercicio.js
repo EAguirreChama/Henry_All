0f28946afed78fa788c24ae1e9f62a892be9f71e
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-28 19:44:59 -0300

    Update Ejercicio.js

--- Modulo 1/Clases/04) Estructura de Datos - Parte 1/Ejercicio.js
+++ Modulo 1/Clases/04) Estructura de Datos - Parte 1/Ejercicio.js
@@ -6,21 +6,23 @@ function contador () {
         name: "FT",
         last: "37a",
         count: 1 
-    };
+    }
 
     let counter = function () { //Es una función
         return obj.count++;
-    };
+    }
     return counter; //Para que sea una closuer no se tiene que haber un llamado a función().
-};
+}
+
 let newCounter = contador ()
-console.log(newCounter()); 
-console.log(newCounter());
-console.log(newCounter());
-console.log(newCounter());
+
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
+console.log(newCounter())
 
 // Otra forma de declarar funciones
 // function counter () {
-//     return obj.count++;
+//     return obj.count++
 // }
 // Es mejor declarar las funciones en constantes

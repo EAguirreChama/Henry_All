47d76f45cb00b8d3031251d73ff9ef78ed6fec72
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-28 19:44:05 -0300

    Update Demo5(Bind).js

--- Modulo 1/Clases/03) Js Avanzado - Parte 2/Demo5(Bind).js
+++ Modulo 1/Clases/03) Js Avanzado - Parte 2/Demo5(Bind).js
@@ -2,9 +2,9 @@
 let persona = {
     nombre: "Ernesto",
     apellido: "Aguirre",
-};
+}
 let logNombre = function () {
-    console.log(this.nombre + " " + this.apellido);
-};
+    console.log(this.nombre + " " + this.apellido)
+}
 let logNombrePersona = logNombre.bind(persona)
 logNombrePersona()

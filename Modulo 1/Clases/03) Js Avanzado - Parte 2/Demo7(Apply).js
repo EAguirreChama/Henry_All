b81ce6e2fd7c3641595b379d2d4a907a1b9c2cff
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-28 19:44:35 -0300

    Update Demo7(Apply).js

--- Modulo 1/Clases/03) Js Avanzado - Parte 2/Demo7(Apply).js
+++ Modulo 1/Clases/03) Js Avanzado - Parte 2/Demo7(Apply).js
@@ -2,10 +2,12 @@
 let persona = {
     nombre: "Ernesto",
     apellido: "Aguirre",
-};
+}
+
 let logNombre = function (arg1, arg2) {
     console.log(arg1 + " " + this.nombre + " " + this.apellido + "," + arg2);
-};
+}
+
 logNombre.apply(persona, ["Hola", " cómo estás?"]) //Un arreglo con parametros
 //Cuando pone "persona" significa que vamos a tomar el valor de la primera variable
 //El resto es un arreglo con argumentos, es decir algo que podemos agregar a la funcion logNombre

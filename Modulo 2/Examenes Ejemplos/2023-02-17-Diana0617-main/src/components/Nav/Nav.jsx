10bffbf90fcf1b5128271dc057ae97e99e90937b
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-29 19:45:18 -0300

    Update Nav.jsx

--- Modulo 2/Examenes Ejemplos/2023-02-17-Diana0617-main/src/components/Nav/Nav.jsx
+++ Modulo 2/Examenes Ejemplos/2023-02-17-Diana0617-main/src/components/Nav/Nav.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-
+import React from "react"
 
 //NO CAMBIEN EL CLASS COMPONENT A FUNCTIONAL COMPONENT PORQUE SINO LOS TEST NO VAN A CORRER!
 export default function Nav() {
     return (
       <div className="nav">
       </div>
-    );
+    )
 }

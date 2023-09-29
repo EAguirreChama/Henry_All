41664c9d47cd790321620e84fa781e2dc57a65a7
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-29 20:19:17 -0300

    Update ShipCard.jsx

--- Modulo 2/Examenes Ejemplos/CP-M2-StarWars(blank)/src/components/ShipCard/ShipCard.jsx
+++ Modulo 2/Examenes Ejemplos/CP-M2-StarWars(blank)/src/components/ShipCard/ShipCard.jsx
@@ -1,6 +1,8 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+
 // Este componente hay que resolverlo de la mano de Ships para hacerlo funcionar correctamente!
 // Si o si este componente debe ser de clase, sino los tests no funcionaran!
+
 export default class ShipCard extends Component {
   constructor(props) {
     super(props)
@@ -9,6 +11,6 @@ export default class ShipCard extends Component {
     return (
       <div>
       </div>
-    );
+    )
   }
-};
+}

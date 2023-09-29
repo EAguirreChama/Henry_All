1208e67c154660689aba2142bdea7811daff9721
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-29 19:40:45 -0300

    Update CharacterCard.jsx

--- Modulo 2/Examenes Ejemplos/2023-02-17-Diana0617-main/src/components/CharacterCard/CharacterCard.jsx
+++ Modulo 2/Examenes Ejemplos/2023-02-17-Diana0617-main/src/components/CharacterCard/CharacterCard.jsx
@@ -1,5 +1,5 @@
-import "./CharacterCard.css";
-import React from "react";
+import React from "react"
+import "./CharacterCard.css"
 // Importar las actions como Object Modules, sino los test no funcionarán!
 
 //PARA QUE LOS TEST CORRAN, DEBEN HACER ESTE COMPONENTE COMO UN FUNCIONAL COMPONENT.
@@ -8,7 +8,7 @@ const CharacterCard = (props) => {
   return (
     <div className="card">
     </div>
-  );
-};
+  )
+}
 
-export default CharacterCard;
+export default CharacterCard

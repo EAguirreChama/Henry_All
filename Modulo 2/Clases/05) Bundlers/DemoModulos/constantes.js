a3c3c700cc2a6aab02c8159b16849b054292a46b
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-02 12:44:24 -0300

    Update constantes.js

--- Modulo 2/Clases/05) Bundlers/DemoModulos/constantes.js
+++ Modulo 2/Clases/05) Bundlers/DemoModulos/constantes.js
@@ -1,9 +1,9 @@
-export const num1 = 5;
-export const num2 = 3;
-export const nombre = "Ernesto";
+export const num1 = 5
+export const num2 = 3
+export const nombre = "Ernesto"
 export const cohorte = () => {
     return "Hola putitos"
-};
+}
 
 // Primera forma de exportar constantes
 // exports.num1 = num1

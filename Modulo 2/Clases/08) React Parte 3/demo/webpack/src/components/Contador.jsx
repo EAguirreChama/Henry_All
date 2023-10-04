808fb369fef17bb825a85ccecdb302d5422b26ce
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-03 21:22:09 -0300

    Update Contador.jsx

--- Modulo 2/Clases/08) React Parte 3/demo/webpack/src/components/Contador.jsx
+++ Modulo 2/Clases/08) React Parte 3/demo/webpack/src/components/Contador.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from 'react'
 
 class Contador extends React.Component {
   constructor(props) {
     super(props);
     this.state = {contador: this.props.contador}
-    this.onButtonClick = this.onButtonClick.bind(this);
-  };
+    this.onButtonClick = this.onButtonClick.bind(this)
+  }
+  
   onButtonClick(e){
-    e.preventDefault();
+    e.preventDefault()
     this.setState({
       contador: this.state.contador + 1,
-    });
-  };
+    })
+  }
+  
   render(){
     return (
       <div>
         <button onClick={this.onButtonClick}>Suma uno!</button>
         <p>Hola, van {this.state.contador}!!</p>
       </div>
-    );
+    )
   }
-};
+}
 
-export default Contador;
+export default Contador

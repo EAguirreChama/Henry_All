a8c534d338e048c674d7483b1224f6176382b27f
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-06 19:50:34 -0300

    Update Contact.jsx

--- Modulo 2/Clases/10) React Parte 05/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ Modulo 2/Clases/10) React Parte 05/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Contact.modules.css'
 
 // eslint-disable-next-line
-const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
 
 export default function Contact () {
   return <div>Crear Formulario</div>

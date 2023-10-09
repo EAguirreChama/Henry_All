849e717df7931841990a17d371ca05e354a6c6ea
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-09 20:42:18 -0300

    Update index.js

--- Modulo 2/Clases/11) Redux/homework/02 - Exercises (vistazo de React)/src/actions/index.js
+++ Modulo 2/Clases/11) Redux/homework/02 - Exercises (vistazo de React)/src/actions/index.js
@@ -1,5 +1,5 @@
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
 
 // Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá. 
 // ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable 
@@ -8,8 +8,8 @@ export const DECREMENT = 'DECREMENT';
 
 export const increment = () => {
   // Completa la funcion
-};
+}
 
 export const decrement = () => {
   // Completa la funcion
-};
+}

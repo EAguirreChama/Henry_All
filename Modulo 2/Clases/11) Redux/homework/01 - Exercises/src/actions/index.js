ed61b931bf75624adc0d964b9f7dd5d8df793298
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-09 20:39:12 -0300

    Update index.js

--- Modulo 2/Clases/11) Redux/homework/01 - Exercises/src/actions/index.js
+++ Modulo 2/Clases/11) Redux/homework/01 - Exercises/src/actions/index.js
@@ -1,4 +1,4 @@
-const { INCREMENTO, DECREMENTO } = require('../action-types');
+const { INCREMENTO, DECREMENTO } = require('../action-types')
 
 // Nuestras actions (action creators) devolverán un paquete de actions que nuestro reducer recibirá. 
 // ¿Cómo es el paquete de acción? Tengan en cuenta que el creador de la acción no es en absoluto responsable 
@@ -9,13 +9,13 @@ const incremento = () => {
   return {
     type: INCREMENTO
   }
-};
+}
 
 const decremento = () => {
   return {
     type: DECREMENTO
   }
-};
+}
 
 module.exports = {
   incremento,

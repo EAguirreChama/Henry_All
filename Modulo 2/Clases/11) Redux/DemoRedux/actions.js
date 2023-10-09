d321aee46980091d8f9d104e304dde46918dfaf2
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-09 20:32:23 -0300

    Update actions.js

--- Modulo 2/Clases/11) Redux/DemoRedux/actions.js
+++ Modulo 2/Clases/11) Redux/DemoRedux/actions.js
@@ -8,15 +8,12 @@ const { AUMENTAR_CONTADOR, CAMBIAR_NOMBRE, MODIFICAR_AMIGOS, MODIFICAR_APELLIDO 
 const aumentarContador = () => {
     return {type: AUMENTAR_CONTADOR}
 }
-
 const cambiarNombre = () => {
     return {type: CAMBIAR_NOMBRE, payload: 'Ernesto'}
 }
-
 const modificarAmigos = () => {
     return {type: MODIFICAR_AMIGOS, payload: ['Fabrizio', 'Alejo', 'Nicolas']}
 }
-
 const modificarApellido = () => {
     return {type: MODIFICAR_APELLIDO, payload: ['Aguirre Chama']}
 }

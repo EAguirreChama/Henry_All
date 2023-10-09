01bfaacc39734ca49b4b80d1499ccf4447572dcf
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-09 20:44:05 -0300

    Update index.js

--- Modulo 2/Clases/11) Redux/homework/02 - Exercises (vistazo de React)/src/index.js
+++ Modulo 2/Clases/11) Redux/homework/02 - Exercises (vistazo de React)/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import Counter from './components/Counter';
-import counter from './reducers';
+import Counter from './components/Counter'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import ReactDOM from 'react-dom'
+import counter from './reducers'
+import React from 'react'
 
 // Esta línea instancia nuestro store central de Redux.
 // La función `createStore` recibe el reducer
 // que es responsable de la actualización del store , junto
 //con cualquier estado inicial con el que queramos que
 //empiece el store (que en este caso es ninguno).
+
 const store = createStore(counter);
 
 // Aquí, envolvemos nuestro componente principal React dentro de las etiquetas del Provider,

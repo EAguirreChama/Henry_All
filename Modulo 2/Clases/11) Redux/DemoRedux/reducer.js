c73ea74e21b74e6e7cf19b4d99c03d618c1f3499
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-09 20:32:52 -0300

    Update reducer.js

--- Modulo 2/Clases/11) Redux/DemoRedux/reducer.js
+++ Modulo 2/Clases/11) Redux/DemoRedux/reducer.js
@@ -8,24 +8,28 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+    
     if (action.type === AUMENTAR_CONTADOR) {
         return{
             ...state,
             contador: state.contador + 1
         }
     }
+    
     if (action.type === CAMBIAR_NOMBRE) {
         return {
             ...state,
             nombre: action.payload
         }
     }
+    
     if (action.type === MODIFICAR_AMIGOS) {
         return {
             ...state,
             amigos: action.payload
         }
     }
+    
     if (action.type === MODIFICAR_APELLIDO) {
         return {
             ...state,

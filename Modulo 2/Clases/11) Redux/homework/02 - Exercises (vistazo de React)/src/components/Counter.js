3c260d79f2c72fc9060384d63595e068d983cd54
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-09 20:42:40 -0300

    Update Counter.js

--- Modulo 2/Clases/11) Redux/homework/02 - Exercises (vistazo de React)/src/components/Counter.js
+++ Modulo 2/Clases/11) Redux/homework/02 - Exercises (vistazo de React)/src/components/Counter.js
@@ -1,12 +1,14 @@
-import { increment, decrement } from "../actions";
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import { increment, decrement } from "../actions"
+import React, { Component } from "react"
+import { connect } from "react-redux"
 
 class Counter extends Component {
+  
   // Extra Credit
   incrementIfOdd = () => {
     //Implementar una función de incremento que sólo aumenta si el valor del contador es impar
   };
+  
   // Extra Credit
   incrementAsync = () => {
     //  Implementar una función de incremento que aumenta después de esperar un segundo

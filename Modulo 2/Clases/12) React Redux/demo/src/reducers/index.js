99f33e9682e35baf4bdf439a79c78fe053cd27b3
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:46:53 -0300

    Update index.js

--- Modulo 2/Clases/12) React Redux/demo/src/reducers/index.js
+++ Modulo 2/Clases/12) React Redux/demo/src/reducers/index.js
@@ -6,32 +6,38 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+      
     case 'INCREMENT':
       return {
         ...state,
         count: state.count + 1,
       }
+      
     case 'DECREMENT':
       return {
         ...state,
         count: state.count - 1,
       }
+      
     case 'RESET':
       return {
         ...state,
         count: 0,
       }
+      
     case 'GET_POST':
       return {
         ...state,
         loading: true,
       }
+      
     case 'RECEIVE_POST':
       return {
         ...state,
         loading: false,
         post: action.post,
       }
+      
     default:
       return {...state}
   }

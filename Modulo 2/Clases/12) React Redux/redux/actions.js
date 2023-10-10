44ba2be9721a3c92081490788d3753fb16762925
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:55:17 -0300

    Update actions.js

--- Modulo 2/Clases/12) React Redux/redux/actions.js
+++ Modulo 2/Clases/12) React Redux/redux/actions.js
@@ -1,11 +1,12 @@
 //incrementar en uno el counter
 const INCREMENTAR_COUNTER = {
  type: 'INCREMENTAR_COUNTER'
-};
+}
+
 // decrementar en uno el counter
 const DECREMENTAR_COUNTER = {
   type: 'DECREMENTAR_COUNTER'
-};
+}
 
 // action creator
 function  increment(n) {
@@ -19,4 +20,4 @@ module.exports = {
   INCREMENTAR_COUNTER,
   DECREMENTAR_COUNTER,
   increment,
-};
+}

8a1a95a96a9145b8355909aa99cbda842ff16293
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:37:53 -0300

    Update store.js

--- Modulo 2/Clases/12) React Redux/DemoReactRedux/demo-reactredux/src/Redux/store.js
+++ Modulo 2/Clases/12) React Redux/DemoReactRedux/demo-reactredux/src/Redux/store.js
@@ -1,8 +1,8 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import thunkMiddleware from "redux-thunk";
-import reducer from "./reducer";
+import { createStore, applyMiddleware, compose } from "redux"
+import thunkMiddleware from "redux-thunk"
+import reducer from "./reducer"
 
-const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose;
+const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose
 
 const store = createStore(
     reducer,

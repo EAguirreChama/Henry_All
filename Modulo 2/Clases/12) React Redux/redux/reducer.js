8ba74b233a3bf39c901a10c9975be9f4f7566c0b
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:56:12 -0300

    Update reducer.js

--- Modulo 2/Clases/12) React Redux/redux/reducer.js
+++ Modulo 2/Clases/12) React Redux/redux/reducer.js
@@ -1,14 +1,18 @@
 const reducer = function(state, action){
   switch(action.type){
+      
     case "INCREMENTAR_COUNTER":
       return state + 1;
+    
     case "DECREMENTAR_COUNTER":
       return state - 1;
+    
     case "INCREMENTAR_N":
       return state + action.payload;
+    
     default:
       return state; // caso por defecto
   }
 }
 
-module.exports =  reducer;
+module.exports =  reducer

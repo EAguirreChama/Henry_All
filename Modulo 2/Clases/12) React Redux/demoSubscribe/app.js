6860232c0784ebd42006aaac47a7c2c37559fb66
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:48:50 -0300

    Update app.js

--- Modulo 2/Clases/12) React Redux/demoSubscribe/app.js
+++ Modulo 2/Clases/12) React Redux/demoSubscribe/app.js
@@ -1,8 +1,8 @@
-import Counter from './src/components/Counter.jsx';
-import  { Provider } from 'react-redux';
-import store from './src/store.js';
-import { render } from 'react-dom';
-import React from 'react';
+import Counter from './src/components/Counter.jsx'
+import  { Provider } from 'react-redux'
+import store from './src/store.js'
+import { render } from 'react-dom'
+import React from 'react'
 
 render(
     <Provider store={store}>

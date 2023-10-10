3cbd765b11bbf77f162aacb63fbdb9c4ea51bb07
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:52:28 -0300

    Update Home.js

--- Modulo 2/Clases/12) React Redux/homework/01 - Exercises/src/Home.js
+++ Modulo 2/Clases/12) React Redux/homework/01 - Exercises/src/Home.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import Products from './components/Products/Products';
 import Form from './components/Form/Form';
+import React from 'react';
 import './home.css';
 
 export default function Home() {

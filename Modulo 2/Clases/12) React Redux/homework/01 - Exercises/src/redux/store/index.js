f00f81ffcbaf5a0ef89dd73457fbf3bd476246df
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:52:06 -0300

    Update index.js

--- Modulo 2/Clases/12) React Redux/homework/01 - Exercises/src/redux/store/index.js
+++ Modulo 2/Clases/12) React Redux/homework/01 - Exercises/src/redux/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import rootReducer from '../reducer';
+import thunk from 'redux-thunk';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // esta línea es para conectar con la extensión del navegador => REDUX DEVTOOLS 
 
@@ -8,4 +8,5 @@ const store = createStore(
     rootReducer,
     composeEnhancer(applyMiddleware(thunk)) // esta línea es para poder hacer peticiones a un server
 );
+
 export default store;

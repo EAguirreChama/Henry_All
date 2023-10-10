340c279d10c74e9418e7b13b609aa221428a652e
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:39:55 -0300

    Update index.js

--- Modulo 2/Clases/12) React Redux/DemoReactRedux/demo-reactredux/src/index.js
+++ Modulo 2/Clases/12) React Redux/DemoReactRedux/demo-reactredux/src/index.js
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'; //Conecta react con redux
-import store from './Redux/store';
-import React from 'react';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux' //Conecta react con redux
+import store from './Redux/store'
+import React from 'react'
+import App from './App'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
-);
+)

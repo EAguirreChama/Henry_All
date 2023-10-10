60b6635790e18f198c8151ca0be118c5671d53f3
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-10 20:43:57 -0300

    Update app.js

--- Modulo 2/Clases/12) React Redux/demo-mutation/app.js
+++ Modulo 2/Clases/12) React Redux/demo-mutation/app.js
@@ -1,8 +1,8 @@
-import Friends from './src/components/Friends.jsx';
-import  { Provider } from 'react-redux';
-import store from './src/store.js';
-import { render } from 'react-dom';
-import React from 'react';
+import Friends from './src/components/Friends.jsx'
+import  { Provider } from 'react-redux'
+import store from './src/store.js'
+import { render } from 'react-dom'
+import React from 'react'
 
 render(
     <Provider store={store}>

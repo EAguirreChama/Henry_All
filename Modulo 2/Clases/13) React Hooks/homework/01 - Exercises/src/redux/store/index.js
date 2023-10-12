ae47149ebd8fb84de7698737cbc8ece13e9a89cd
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:32:14 -0300

    Update index.js

--- Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/src/redux/store/index.js
+++ Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/src/redux/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import rootReducer from '../reducer/reducer';
+import thunk from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
-export default store;
+export default store

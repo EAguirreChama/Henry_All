db2f586447b0a731e3aaae600f3bcee4a02cb8b5
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:35:52 -0300

    Update 02.test.js

--- Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/tests/02.test.js
+++ Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/tests/02.test.js
@@ -8,7 +8,7 @@ import { mount, configure } from "enzyme"
 import { Provider } from "react-redux"
 import React from "react"
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
 describe("01 | Ejercicios", () => {
   const mockStore = configureStore();
@@ -17,7 +17,7 @@ describe("01 | Ejercicios", () => {
       expect(actions.enviarForm(undefined)).toEqual({
         type: "FORM_DATA",
         payload: undefined,
-      });
+      })
       expect(
         actions.enviarForm({
           nombre: "Mateo Hernandez",
@@ -33,33 +33,33 @@ describe("01 | Ejercicios", () => {
           asunto: "Postulacion",
           mensaje: "Me gustaria postularme como full stack developer",
         },
-      });
-    });
-  });
+      })
+    })
+  })
   describe("Reducer", () => {
     it("Debería retornar el estado global con el payload agregado cuando el caso es FORM_DATA", () => {
       const state = {
         formulario: {},
-      };
+      }
       const payload = {
         nombre: "Martin Precelle",
         email: "mprecelle@gmail.com",
         asunto: "Postulacion",
         mensaje: "Me gustaria postularme como full stack developer",
-      };
+      }
       const firstForm = reducer(state, actions.enviarForm(payload));
       // Recordar que el store de redux no debe mutar!
-      expect(firstForm).not.toEqual(state);
+      expect(firstForm).not.toEqual(state)
       expect(firstForm).toEqual({
         formulario: payload,
-      });
-    });
+      })
+    })
     it("Debería retornar el estado cuando el caso es default", () => {
       expect(reducer(undefined, actions.enviarForm({}))).toEqual({
         formulario: {},
-      });
-    });
-  });
+      })
+    })
+  })
   describe("Dispatch enviarForm", () => {
     it("Debería invocarse el hook dispatch para poder despachar la action 'enviarFormulario'", () => {
       const store = mockStore({});
@@ -67,7 +67,7 @@ describe("01 | Ejercicios", () => {
         <Provider store={store}>
           <ContactUs />
         </Provider>
-      );
+      )
       expect(contactUs).toBeTruthy();
       const enviarFormSpy = jest.spyOn(actions, "enviarForm");
       contactUs.find("button").simulate("click");
@@ -78,7 +78,7 @@ describe("01 | Ejercicios", () => {
         email: "",
         asunto: "",
         mensaje: "",
-      });
-    });
-  });
-});
+      })
+    })
+  })
+})

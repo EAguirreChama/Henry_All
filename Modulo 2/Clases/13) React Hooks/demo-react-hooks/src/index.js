ee0fca6086217438f543b419d01530ca8cc8d172
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:25:53 -0300

    Update index.js

--- Modulo 2/Clases/13) React Hooks/demo-react-hooks/src/index.js
+++ Modulo 2/Clases/13) React Hooks/demo-react-hooks/src/index.js
@@ -1,9 +1,9 @@
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './Redux/Store';
-import React from 'react';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './Redux/Store'
+import React from 'react'
+import App from './App'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

aba2fddf4702efbeb7a77d45156d67022dba2d30
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:23:49 -0300

    Update Store.js

--- Modulo 2/Clases/13) React Hooks/demo-react-hooks/src/Redux/Store.js
+++ Modulo 2/Clases/13) React Hooks/demo-react-hooks/src/Redux/Store.js
@@ -1,6 +1,6 @@
-import {createStore, applyMiddleware, compose} from "redux";
+import {createStore, applyMiddleware, compose} from "redux"
 import thunkMiddleware from "redux-thunk"
-import reducer from "./Reducer";
+import reducer from "./Reducer"
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 

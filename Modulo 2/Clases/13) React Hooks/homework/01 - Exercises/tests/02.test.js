f58a05473f205284f71dbc02052d32f9930dac40
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:35:10 -0300

    Update 02.test.js

--- Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/tests/02.test.js
+++ Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/tests/02.test.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-import { mount, configure } from "enzyme";
+import ContactUs from "../src/components/ContactUs/ContactUs";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import * as actions from "../src/redux/actions/actions";
 import reducer from "../src/redux/reducer/reducer";
-import ContactUs from "../src/components/ContactUs/ContactUs";
+import "@testing-library/jest-dom/extend-expect";
 import configureStore from "redux-mock-store";
+import { mount, configure } from "enzyme";
 import { Provider } from "react-redux";
+import React from "react";
 
 configure({ adapter: new Adapter() });
 

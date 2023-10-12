3a1c6531dafbf8de572e765a63431a25abf33ff2
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:32:01 -0300

    Update reducer.js

--- Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/src/redux/reducer/reducer.js
+++ Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/src/redux/reducer/reducer.js
@@ -1,7 +1,7 @@
 const initialState = {
   formulario: {},
-};
+}
 
-const rootReducer = (state = initialState, { type, payload }) => {};
+const rootReducer = (state = initialState, { type, payload }) => {}
 
-export default rootReducer;
+export default rootReducer

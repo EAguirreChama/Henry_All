ca3060adf77c442d25174f563275788ca20d6ac1
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:21:01 -0300

    Update Characters.jsx

--- Modulo 2/Clases/13) React Hooks/demo-react-hooks/src/Componentes/Characters.jsx
+++ Modulo 2/Clases/13) React Hooks/demo-react-hooks/src/Componentes/Characters.jsx
@@ -2,7 +2,6 @@ import { getCharacters, cleanCharacters} from "../Redux/Action";
 import {useDispatch, useSelector} from "react-redux"
 import { useEffect } from "react";
 
-
 const Characters = () => {
     const Dispatch = useDispatch();
     const characters = useSelector(state => state.characters);

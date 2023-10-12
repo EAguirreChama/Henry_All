edc7c92572e37a06fe2377a28b1ff254d857c297
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:29:25 -0300

    Update index.js

--- Modulo 2/Clases/13) React Hooks/demo/src/index.js
+++ Modulo 2/Clases/13) React Hooks/demo/src/index.js
@@ -1,18 +1,18 @@
-import App from './components/App';
-import ReactDOM from 'react-dom';
-import React from 'react';
-import './index.css';
+import App from './components/App'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import './index.css'
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
-import reducer from './reducer';
+import reducer from './reducer'
 
-const store = createStore(reducer);
+const store = createStore(reducer)
 
 ReactDOM.render(
   <Provider store={store} >
     <App />
   </Provider>,
   document.getElementById('root')
-);
+)

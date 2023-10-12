4ebe4b56006333349877f6fb2c60c5fe5f8601ed
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-10-12 15:38:53 -0300

    Update 05.test.js

--- Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/tests/05.test.js
+++ Modulo 2/Clases/13) React Hooks/homework/01 - Exercises/tests/05.test.js
@@ -1,18 +1,18 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect";
-import { shallow, configure } from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import CopyData from "../src/components/CopyData/CopyData";
+import CopyData from "../src/components/CopyData/CopyData"
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17"
+import "@testing-library/jest-dom/extend-expect"
+import { shallow, configure } from "enzyme"
+import React from "react"
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
 describe("05 | Ejercicios", () => {
   let copyData;
   beforeEach(() => {
     copyData = shallow(<CopyData />);
-  });
+  }
 
-  afterEach(() => jest.restoreAllMocks());
+  afterEach(() => jest.restoreAllMocks())
 
   it("Debe declarar un estado local que sea inicializado en un string", () => {
     const phoneRegex = /([0-9]{3})-([0-9]{3})-([0-9]{4})/;
@@ -21,17 +21,17 @@ describe("05 | Ejercicios", () => {
     expect(useStateSpy).toHaveBeenCalled();
     // NOTA: el estado local debe tener si o si el formato XXX-XXX-XXXX sino el test fallara
     expect(phoneRegex.test(useStateSpy.mock.calls[0])).toEqual(true);
-  });
+  })
 
   it("Debe invocar y guardar en una variable el valor del hook React.useRef()", () => {
     const useRefSpy = jest.spyOn(React, "useRef");
     shallow(<CopyData />);
     expect(useRefSpy).toHaveBeenCalled();
-  });
+  })
 
   it('Debe renderizar una etiqueta button con el text "Copy"', () => {
     expect(copyData.find("button").text()).toEqual("Copy");
-  });
+  })
 
   it('Debe renderizar un div con la propiedad "ref" haciendo referencia a useRef, con el texto "TELEFONO + <estadoLocal>"', () => {
     const useStateSpy = jest.spyOn(React, "useState");
@@ -44,9 +44,9 @@ describe("05 | Ejercicios", () => {
         .children.join("")
         .includes(useStateSpy.mock.calls[0])
     ).toEqual(true);
-  });
+  })
 
   it('El button debe recibir dentro del evento "onClick", la función "handleCopy"', () => {
     expect(copyData.find("button").props().onClick).toBeInstanceOf(Function);
-  });
-});
+  })
+})
